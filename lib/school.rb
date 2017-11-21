2a8349fa5879e70be72e700d24235f30ed1ecda2
github username <mitch@mitchboyer.com>
2017-11-20 20:52:42 -0700

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
 # code here!
+class School
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade] << name
+    else
+      @roster[grade]= [name]
+    end
+  end
+
+  def grade(grade)
+    @roster.map { |g, s| s if g == grade }.compact.flatten
+  end
+
+  def sort
+    @roster.each { |g, s| s.sort! }
+  end
+end
